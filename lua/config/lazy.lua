6b12e88ea61a5d29f509a2d88ac331ac881c50b8
Chompou <tobgiver@gmail.com>
2024-11-09 19:40:03 +0100

    Rust LSP

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,9 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- rust lsp
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- import/override with your plugins
     { import = "plugins" },
   },
